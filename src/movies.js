34544151a043596e258e6cbb20dd01c69510e995
Laura Santos <alsr16p@gmail.com>
2021-01-14 20:53:02 +0000

    done

--- src/movies.js
+++ src/movies.js
@@ -18,11 +18,51 @@ let amountOfMovies = movieArray.filter((movie) => {
 
 return amountOfMovies.length;
 });
+
+
+
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage(movies) {
+    if (movies.length === 0) {
+        return 0  
+    } 
+let total = movies.reduce((accumulator, currentValue) => {
+    if (currentValue.rate) {
+        return accumulator + currentValue.rate;
+    } else {
+        return accumulator
+    }
+}, 0)
+
+return Number((total/movies.length).toFixed(2)); 
+
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(movies) {
+    let dramaMovies = movies.filter((movie) => movie.genre.includes('Drama'));
+    return ratesAverage(dramaMovies);
+}
+
+
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+            //teacher code to check if it goes green
+function orderByYear(movies) {
+    return movies.sort((movie1, movie2) => {
+
+            if (movie1.year === movie2.year){
+                //order by title
+            return movie1.title.localeCompare(movie2.title); //this is a little trick to check
+            }else {
+                return movie1.year - movie2.year; //little trick
+            }
+    }).map((item) => {
+        return item;
+    })
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
